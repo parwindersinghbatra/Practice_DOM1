bd1fe11e46d4b43843e37ca9bee7a816eefcde31
parwindersinghbatra <parwinder.singhbatra@outlook.com>
2024-07-02 18:51:56 -0400

    First Practice

---
+++ script.js
@@ -0,0 +1,10 @@
+const result = document.getElementById("container");
+
+result.style.cssText= 'font-Size:18px;'
+result.style.backgroundColor = 'yellow'
+result.style.height = '200px'
+result.style.width = '200px'
+result.style.marginLeft = '20px'
+result.style.paddingLeft = '20px'
+result.style.paddingTop = '20px'
+result.style.fontWeight='bold'
